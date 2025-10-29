275ebb2bc25da5ed145fb25885a4c3f510977969
cheonHwi <hoba0409@naver.com>
2025-10-30 01:08:01 +0900

    feat(Application): 입력부, 로또 생성부 구현
    - InputService로 숫자데이터 입력
    - IntStream을 이용해 구입 금액만큼 로또 생성
    - 생성 이후 로또 번호 출력 후 로또 리스트에 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,34 @@
 package lotto;
 
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputService inputService = new InputService();
+
+        System.out.println("구입금액을 입력해 주세요.");
+        int money = inputService.inputNumber();
+
+        if(money % 1000 != 0) {
+            throw new IllegalArgumentException("로또 구입 금액은 1,000으로 나누어 떨어져야 합니다.");
+        }
+
+        int lottoNumbers = money / 1000;
+        List<Lotto> lottos = new ArrayList<>();
+
+        System.out.println(lottoNumbers + "개를 구매했습니다.");
+        IntStream.range(0, lottoNumbers)
+                .forEach(index -> {
+                    Lotto lotto = new Lotto(
+                            Randoms.pickUniqueNumbersInRange(1, 45, 6)
+                    );
+                    lotto.getLottoNumbers();
+                    lottos.add(lotto);
+                });
     }
 }
