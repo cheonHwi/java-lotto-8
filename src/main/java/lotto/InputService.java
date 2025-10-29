7d32d8745ee94e77dcdb9c85dc35551382d5c185
cheonHwi <hoba0409@naver.com>
2025-10-29 22:37:10 +0900

    refactor: 숫자 입력 함수 리팩토링
    - inputNumber : NumberFormatException을 IllegalArgumentException로 바꾸는 로직 제거, 단순 재귀로 수정

--- src/main/java/lotto/InputService.java
+++ src/main/java/lotto/InputService.java
@@ -4,21 +4,11 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputService {
     public int inputNumber() {
-        int number = 0;
         try {
-            String numberInput = Console.readLine();
-            try {
-                number = Integer.parseInt(numberInput);
-            }
-            catch (NumberFormatException e) {
-                throw new IllegalArgumentException("input must be an integer");
-            }
-        }
-        catch (IllegalArgumentException e) {
+            return Integer.parseInt(Console.readLine());
+        } catch (NumberFormatException e) {
             System.out.println("[ERROR]");
-            inputNumber();
+            return inputNumber();
         }
-
-        return number;
     }
 }
