9a12db807973e803f7fd0a9a6673cb4a8e9f74b5
cheonHwi <hoba0409@naver.com>
2025-10-30 01:23:19 +0900

    feat(Lotto): 로또 번호 출력 함수 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -15,6 +17,15 @@ public class Lotto {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
+
+    public void getLottoNumbers() {
+        String winnerString = numbers.stream()
+                .map(integer -> Integer.toString(integer))
+                .collect(Collectors.joining(", "));
+
+        System.out.println("[" + winnerString + "]");
+    }
+
 
     // TODO: 추가 기능 구현
 }
