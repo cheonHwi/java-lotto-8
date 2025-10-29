f0e1d77276da53549db7a9fd940fd796fbb13342
cheonHwi <hoba0409@naver.com>
2025-10-30 01:23:44 +0900

    test(Lotto): 로또 번호 출력 검증 테스트 작성

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,13 +1,32 @@
 package lotto;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LottoTest {
+    final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    final PrintStream standardOut = System.out;
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(standardOut);
+    }
+
+
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
@@ -20,6 +39,17 @@ class LottoTest {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+
+    @Test
+    void 로또가_생성될때_로또_번호를_출력한다() {
+        String correctOutput = "[1, 2, 3, 4, 5, 6]";
+
+        new Lotto(List.of(1, 2, 3, 4, 5, 6)).getLottoNumbers();
+
+        assertEquals(correctOutput, outputStreamCaptor.toString().trim());
+    }
+
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
 }
