fe6208208cd368773da61a909e832de15afc9e83
cheonHwi <hoba0409@naver.com>
2025-11-01 15:15:30 +0900

    feat(Application) 로또 구매, 당첨번호, 보너스 번호 입력 lottoService 에서 가져와 사용하도록 수정

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,34 +1,34 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.IntStream;
 
 public class Application {
     public static void main(String[] args) {
         InputService inputService = new InputService();
+        LottoService lottoService = new LottoService();
+
+        int money;
+        int bonusNumber;
 
         System.out.println("구입금액을 입력해 주세요.");
-        int money = inputService.inputNumber();
+        money = inputService.inputNumber();
 
         if(money % 1000 != 0) {
             throw new IllegalArgumentException("로또 구입 금액은 1,000으로 나누어 떨어져야 합니다.");
         }
 
         int lottoNumbers = money / 1000;
-        List<Lotto> lottos = new ArrayList<>();
 
         System.out.println(lottoNumbers + "개를 구매했습니다.");
-        IntStream.range(0, lottoNumbers)
-                .forEach(index -> {
-                    Lotto lotto = new Lotto(
-                            Randoms.pickUniqueNumbersInRange(1, 45, 6)
-                    );
-                    lotto.getLottoNumbers();
-                    lottos.add(lotto);
-                });
+        List<Lotto> lottos = lottoService.getLottos(lottoNumbers);
+
+
+        System.out.println("당첨 번호를 입력해 주세요.");
+        List<Integer> winningNumber = lottoService.getWinningNumbers(Console.readLine());
+
+        System.out.println("보너스 번호를 입력해 주세요.");
+        bonusNumber = inputService.inputNumber();
     }
 }
