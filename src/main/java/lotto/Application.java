50b3f1e4f131751fa00e2ba4978c83433dea854b
cheonHwi <hoba0409@naver.com>
2025-11-03 22:27:13 +0900

    feat(Application) 당첨 로또, 수익률 확인 로직 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -3,17 +3,15 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.List;
+import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
         InputService inputService = new InputService();
         LottoService lottoService = new LottoService();
 
-        int money;
-        int bonusNumber;
-
         System.out.println("구입금액을 입력해 주세요.");
-        money = inputService.inputNumber();
+        int money = inputService.inputNumber();
 
         if(money % 1000 != 0) {
             throw new IllegalArgumentException("로또 구입 금액은 1,000으로 나누어 떨어져야 합니다.");
@@ -24,11 +22,15 @@ public class Application {
         System.out.println(lottoNumbers + "개를 구매했습니다.");
         List<Lotto> lottos = lottoService.getLottos(lottoNumbers);
 
-
         System.out.println("당첨 번호를 입력해 주세요.");
         List<Integer> winningNumber = lottoService.getWinningNumbers(Console.readLine());
 
         System.out.println("보너스 번호를 입력해 주세요.");
-        bonusNumber = inputService.inputNumber();
+        int bonusNumber = inputService.inputNumber();
+
+        Map<WinningRank, Long> statistics = lottoService.calculateStatistics(lottos, winningNumber, bonusNumber);
+        double returnRate = lottoService.calculateReturnRate(statistics, money);
+
+        lottoService.printStatistics(statistics, returnRate);
     }
 }
