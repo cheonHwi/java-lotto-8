7cd57c526a2878eebb22345ca3da768fc5c0a6a2
cheonHwi <hoba0409@naver.com>
2025-11-03 19:02:04 +0900

    test(Lotto) 로또 번호 검증 테스트 케이스 추가

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -27,9 +27,24 @@ class LottoTest {
     }
 
 
+    @DisplayName("로또 번호의 개수가 6개가 넘어가면 예외가 발생한다.")
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호의 개수가 6개 미만이면 예외가 발생한다.")
+    @Test
+    void 로또_번호의_개수가_6개_미만이면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5)))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 번호가 비어있으면 예외가 발생한다.")
+    @Test
+    void 로또_번호가_비어있으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of()))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -40,7 +55,7 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-
+    @DisplayName("로또가 생성될 때 로또 번호를 올바른 형식으로 출력한다.")
     @Test
     void 로또가_생성될때_로또_번호를_출력한다() {
         String correctOutput = "[1, 2, 3, 4, 5, 6]";
@@ -50,8 +65,7 @@ class LottoTest {
         assertEquals(correctOutput, outputStreamCaptor.toString().trim());
     }
 
-
-    // 번호 하나하나 검증하는 함수는 private로 작성되어 검증 함수를 실제로 사용하는 confirmWinningNumbers로 테스트한다.
+    @DisplayName("로또 번호가 당첨 번호 및 보너스 번호와 정확히 매칭된다.")
     @Test
     void 로또_번호_검증() {
         Lotto lotto = new Lotto(List.of(1, 2, 3, 10, 11, 7));
@@ -69,5 +83,61 @@ class LottoTest {
         assertEquals(correctMatchResult, matchResult);
     }
 
+    @DisplayName("로또 번호가 당첨 번호와 하나도 일치하지 않는 경우를 확인한다.")
+    @Test
+    void 로또_번호가_당첨번호와_하나도_일치하지_않는_경우() {
+        Lotto lotto = new Lotto(List.of(10, 11, 12, 13, 14, 15));
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 7;
+
+        List<LottoNumberMatchType> matchResult = lotto.confirmWinningNumbers(winningNumbers, bonusNumber);
+        List<LottoNumberMatchType> correctMatchResult = List.of(
+                LottoNumberMatchType.INCORRECT,
+                LottoNumberMatchType.INCORRECT,
+                LottoNumberMatchType.INCORRECT,
+                LottoNumberMatchType.INCORRECT,
+                LottoNumberMatchType.INCORRECT,
+                LottoNumberMatchType.INCORRECT);
+
+        assertEquals(correctMatchResult, matchResult);
+    }
+
+    @DisplayName("로또 번호가 당첨 번호와 모두 일치하는 경우를 확인한다.")
+    @Test
+    void 로또_번호가_당첨번호와_모두_일치하는_경우() {
+        Lotto lotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 7;
+
+        List<LottoNumberMatchType> matchResult = lotto.confirmWinningNumbers(winningNumbers, bonusNumber);
+        List<LottoNumberMatchType> correctMatchResult = List.of(
+                LottoNumberMatchType.CORRECT,
+                LottoNumberMatchType.CORRECT,
+                LottoNumberMatchType.CORRECT,
+                LottoNumberMatchType.CORRECT,
+                LottoNumberMatchType.CORRECT,
+                LottoNumberMatchType.CORRECT);
+
+        assertEquals(correctMatchResult, matchResult);
+    }
+
+    @DisplayName("로또 번호가 보너스 번호만 일치하는 경우를 확인한다.")
+    @Test
+    void 로또_번호가_보너스번호만_일치하는_경우() {
+        Lotto lotto = new Lotto(List.of(7, 10, 11, 12, 13, 14));
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        int bonusNumber = 7;
+
+        List<LottoNumberMatchType> matchResult = lotto.confirmWinningNumbers(winningNumbers, bonusNumber);
+        List<LottoNumberMatchType> correctMatchResult = List.of(
+                LottoNumberMatchType.BONUS_NUMBER_CORRECT,
+                LottoNumberMatchType.INCORRECT,
+                LottoNumberMatchType.INCORRECT,
+                LottoNumberMatchType.INCORRECT,
+                LottoNumberMatchType.INCORRECT,
+                LottoNumberMatchType.INCORRECT);
+
+        assertEquals(correctMatchResult, matchResult);
+    }
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
 }
